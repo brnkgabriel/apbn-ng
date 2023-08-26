ec9bccb21f96005b7cfc6c7ed51586b32b9fc2cb
bribironke <br.ibironke@gmail.com>
2023-08-26 23:16:06 +0100

    collection update

--- src/content/config.ts
+++ src/content/config.ts
@@ -37,6 +37,25 @@ const TeamCollection = defineCollection({
   })
 })
 
+const SlidersCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    active: z.boolean(),
+    date: z.string(),
+    name: z.string(),
+    time: z.string(),
+    venue: z.string(),
+    theme: z.string(),
+    host: z.string(),
+    chairman: z.string(),
+    keynotespeaker: z.string(),
+    videourl: z.string().optional(),
+    image: z.string(),
+    type: z.string(),
+    body: z.string().optional()
+  })
+})
+
 export const collections = {
   "members": MembersCollection,
   "events": EventsCollection,
